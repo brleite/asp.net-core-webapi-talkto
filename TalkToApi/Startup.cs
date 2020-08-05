cd5fe8cbd12c833b7f80539336344f5b7788d010
Bruno Mayerle Leite <brleite@gmail.com>
2020-08-05 14:52:20 -0300

    Cadastro e Token

--- TalkToApi/Startup.cs
+++ TalkToApi/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using TalkToApi.Database;
 
 namespace TalkToApi
 {
@@ -25,6 +27,10 @@ namespace TalkToApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TalkToContext>(options =>
+                options.UseMySql(Configuration.GetConnectionString("TalkToContext"), builder =>
+                    builder.MigrationsAssembly("TalkToApi")));
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
